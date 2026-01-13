1c75faebaf2986fe466c99336dc5e91d65ed36c6
Vinay Reddy <lekkalavinayreddy@gmail.com>
2026-01-12 19:06:52 -0500

    Delete ui directory - to test UI code

--- ui/src/lib/tenant-context.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { createContext, useContext } from "react";
-
-export const TenantContext = createContext<string | null>(null);
-
-export function useTenant() {
-  return useContext(TenantContext);
-}
